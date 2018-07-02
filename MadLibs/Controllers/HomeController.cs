fdfe84a51ccb9854a08e5854bb7717589ed110c2
Student <example@example.com>
2018-07-02 10:34:34 -0700

    Add initial commit with complete project

---
+++ MadLibs/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MadLibs.Models;
+
+namespace MadLibs.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public ActionResult Form()
+        { 
+            return View(); 
+        }
+
+        [Route("/result")]
+        public ActionResult Result()
+        {
+            WordVariables myWordVariable = new WordVariables();
+            myWordVariable.SetName(Request.Query["name"]);
+            myWordVariable.SetAdjOne(Request.Query["adjOne"]);
+            myWordVariable.SetVerbOne(Request.Query["verbOne"]);
+
+            myWordVariable.SetNounOne(Request.Query["nounOne"]);
+            myWordVariable.SetVerbTwo(Request.Query["verbTwo"]);
+            myWordVariable.SetNounTwo(Request.Query["nounTwo"]);
+
+            myWordVariable.SetVerbThree(Request.Query["verbThree"]);
+            myWordVariable.SetNounThree(Request.Query["nounThree"]);
+            myWordVariable.SetAdverb(Request.Query["adverb"]);
+
+            return View(myWordVariable);
+        }
+
+    }
+}
---
+++ MadLibs/Models/WordVariables.cs
@@ -0,0 +1,106 @@
+﻿using System;
+namespace MadLibs.Models
+{
+    public class WordVariables
+    {
+        private string _name;
+        private string _adjOne;
+        private string _verbOne;
+        private string _nounOne;
+        private string _verbTwp;
+        private string _nounTwo;
+        private string _verbThree;
+        private string _nounThree;
+        private string _adverb;
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetName(string newName)
+        {
+            _name = newName;
+        }
+
+        public string GetAdjOne()
+        {
+            return _adjOne;
+        }
+
+        public void SetAdjOne(string newAdj)
+        {
+            _adjOne = newAdj;
+        }
+
+        public string GetVerbOne()
+        {
+            return _verbOne;
+        }
+
+        public void SetVerbOne(string newVerb)
+        {
+            _verbOne = newVerb;
+        }
+
+        public string GetNounOne()
+        {
+            return _nounOne;
+        }
+
+        public void SetNounOne(string newNoun)
+        {
+            _nounOne = newNoun;
+        }
+
+        public string GetVerbTwo()
+        {
+            return _verbTwp;
+        }
+
+        public void SetVerbTwo(string newVerb)
+        {
+            _verbTwp = newVerb;
+        }
+
+        public string GetNounTwo()
+        {
+            return _nounTwo;
+        }
+
+        public void SetNounTwo(string newNoun)
+        {
+            _nounTwo = newNoun;
+        }
+
+        public string GetVerbThree()
+        {
+            return _verbThree;
+        }
+
+        public void SetVerbThree(string newVerb)
+        {
+            _verbThree = newVerb;
+        }
+
+        public string GetNounThree()
+        {
+            return _nounThree;
+        }
+
+        public void SetNounThree(string newNoun)
+        {
+            _nounThree = newNoun;
+        }
+
+        public string GetAdverb()
+        {
+            return _adverb;
+        }
+
+        public void SetAdverb(string newAdverb)
+        {
+            _adverb = newAdverb;
+        }
+    }
+}
